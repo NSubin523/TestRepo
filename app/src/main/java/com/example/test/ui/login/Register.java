99a97b5b55fdfb0c79456d15c1c69652290408c9
Subin Neupane <74434319+NSubin523@users.noreply.github.com>
2021-02-02 19:36:58 -0500

    Update Register.java

--- app/src/main/java/com/example/test/ui/login/Register.java
+++ app/src/main/java/com/example/test/ui/login/Register.java
@@ -1,11 +1,15 @@
 package com.example.test.ui.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.test.R;
 
@@ -15,14 +19,20 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        EditText firstName = findViewById(R.id.editTextTextPersonName);
+        EditText lastName = findViewById(R.id.editTextTextPersonName2);
         Button regUserInfo= findViewById(R.id.registerInfo);
         regUserInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Register.this,LoginActivity.class));
-            }
+                    if(!TextUtils.isEmpty(firstName.getText()) && !TextUtils.isEmpty(lastName.getText()))
+                        startActivity(new Intent(Register.this,LoginActivity.class));
+                    else
+                        Toast.makeText(getApplicationContext(),"Fill login credentials", Toast.LENGTH_SHORT).show();
+                }
         });
+
+
     }
 
 }
